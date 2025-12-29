ebb6037aa04a2125f9cfa8bed97620b95876ca9f
Qori Chairawan <qorichairawan17@gmail.com>
2025-12-29 12:19:40 +0700

    feat: add middleware to ensure user profile is complete

--- app/Http/Middleware/Profile/CompleteProfileMiddleware.php
+++ app/Http/Middleware/Profile/CompleteProfileMiddleware.php
@@ -17,24 +17,24 @@ class CompleteProfileMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // /** @var \App\Models\User $user */
-        // $user = Auth::user();
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
 
-        // // It's better to move this logic into a method on the User model,
-        // // for example: $user->isProfileComplete()
-        // $profile = $user->profile;
+        // It's better to move this logic into a method on the User model,
+        // for example: $user->isProfileComplete()
+        $profile = $user->profile;
 
-        // if (
-        //     !$profile ||
-        //     !$profile->tanggal_lahir ||
-        //     !$profile->jenis_kelamin ||
-        //     !$profile->kontak ||
-        //     !$profile->alamat ||
-        //     !$profile->foto ||
-        //     $user->profile_status != 1
-        // ) {
-        //     return redirect()->route('profile.index')->with('warning', 'Lengkapi profil Kamu terlebih dahulu.');
-        // }
+        if (
+            !$profile ||
+            !$profile->tanggal_lahir ||
+            !$profile->jenis_kelamin ||
+            !$profile->kontak ||
+            !$profile->alamat ||
+            !$profile->foto ||
+            $user->profile_status != 1
+        ) {
+            return redirect()->route('profile.index')->with('warning', 'Lengkapi profil Kamu terlebih dahulu.');
+        }
 
         return $next($request);
     }
